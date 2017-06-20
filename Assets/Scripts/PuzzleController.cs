5633e305df6a60014ca6f54a2b23df191753f832
hamada.kenji <hamada.kenji0925@gmail.com>
2017-06-20 14:25:46 +0900

    PuzzleSpinnerプロジェクト　　GameController.cs修正
    パズル生成処理を修正

--- Assets/Scripts/PuzzleController.cs
+++ Assets/Scripts/PuzzleController.cs
@@ -3,6 +3,29 @@ using System.Collections;
 
 public class PuzzleController : MonoBehaviour {
 
+//Prefabで管理してみる
+//	//パズル格納用二次元配列
+//	private GameObject[,] PuzzleBlockAry;
+//	public Sprite[] PuzzleSprite;
+//
+//	//パズル縦・横数指定
+//	public int PuzzleX;
+//	public int PuzzleY;
+//
+//	void Start () {
+//		//Inspectorで指定された縦＊横で二次元配列宣言
+//		this.PuzzleBlockAry = new GameObject[this.PuzzleY,this.PuzzleX];
+//
+//		//宣言された二次元配列にランダムに[Piece]sprite格納
+//		for (int i = 0; i < this.PuzzleY; i++) {
+//			for (int j = 0; j < this.PuzzleX; j++) {
+//				//PuzzleBlockAry配列に[Piece]spriteの中身をランダムに格納
+//				//this.PuzzleBlockAry [i, j] = this.PuzzleSprite[Random.Range(0,this.PuzzleSprite.Length-1)];
+//			}
+//		}
+//	
+//	}
+
 	//パズル管理-二次元配列宣言-
 	private Sprite[,] PuzzleBlockAry;
 	//Sprite格納用
@@ -12,8 +35,8 @@ public class PuzzleController : MonoBehaviour {
 	public int PuzzleX = 1;
 	public int PuzzleY = 1;
 
-	//パズル二次元配列-要素数格納-
-	//public Vector2 Puzzle_ver_sid;
+	//spriteのGameObjectを宣言
+	public GameObject gameObject;
 
 	// Use this for initialization
 	void Start () {
@@ -30,9 +53,15 @@ public class PuzzleController : MonoBehaviour {
 			}
 		}
 
-		Debug.Log (this.PuzzleBlockAry[0,0]);
+		//Prefabを生成
+		this.gameObject = (GameObject)Resources.Load (this.PuzzleBlockAry [0, 0].name);
 
+		Debug.Log (this.PuzzleBlockAry[0,0].name);
 
+		GameObject go = Instantiate (gameObject) as GameObject;
+
+//		
+//		go.transform.position = new Vector2 (0, 0);
 	}
 
 	// Update is called once per frame
--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -6,13 +6,29 @@ public class test : MonoBehaviour {
 //	private Sprite TestSprite;
 
 	// Use this for initialization
-	void Start () {
-//		this.TestSprite = Resources.Load<Sprite> ("Piece");
-//		Debug.Log (this.TestSprite.name);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+//	void Start () {
+//		public GameObject ballPrefab;
+//		public Sprite[] ballSprites;
+//
+//		void Start () {
+//			StartCoroutine(DropBall(50));
+//		}
+//
+//		IEnumerator DropBall(int count) {
+//			for (int i = 0; i < count; i++) {
+//				Vector2 pos = new Vector2(Random.Range(-2.0f, 2.0f), 7f);
+//				GameObject ball = Instantiate(ballPrefab, pos, Quaternion.AngleAxis(Random.Range(-40, 40), Vector3.forward)) as GameObject;
+//				int spriteId = Random.Range(0, 5);
+//				ball.name = "Piyo" + spriteId;
+//				SpriteRenderer spriteObj = ball.GetComponent<SpriteRenderer>();
+//				spriteObj.sprite = ballSprites[spriteId];
+//				yield return new WaitForSeconds(0.05f);
+//			}
+//		}
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
 }
