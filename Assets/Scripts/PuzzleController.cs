fb4476cabcb79dde619b1298dc8d003871f69d97
hamada.kenji <hamada.kenji0925@gmail.com>
2017-06-20 17:57:26 +0900

    PuzzleSpinnerプロジェクト　　Unityのバージョン変更
    バージョンを4.6.1にあげ、メンタリング後の変更内容を反映

--- Assets/Scripts/PuzzleController.cs
+++ Assets/Scripts/PuzzleController.cs
@@ -1,76 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PuzzleController : MonoBehaviour {
-
-//Prefabで管理してみる
-//	//パズル格納用二次元配列
-//	private GameObject[,] PuzzleBlockAry;
-//	public Sprite[] PuzzleSprite;
-//
-//	//パズル縦・横数指定
-//	public int PuzzleX;
-//	public int PuzzleY;
-//
-//	void Start () {
-//		//Inspectorで指定された縦＊横で二次元配列宣言
-//		this.PuzzleBlockAry = new GameObject[this.PuzzleY,this.PuzzleX];
-//
-//		//宣言された二次元配列にランダムに[Piece]sprite格納
-//		for (int i = 0; i < this.PuzzleY; i++) {
-//			for (int j = 0; j < this.PuzzleX; j++) {
-//				//PuzzleBlockAry配列に[Piece]spriteの中身をランダムに格納
-//				//this.PuzzleBlockAry [i, j] = this.PuzzleSprite[Random.Range(0,this.PuzzleSprite.Length-1)];
-//			}
-//		}
-//	
-//	}
+public class PuzzleController : MonoBehaviour
+{
 
 	//パズル管理-二次元配列宣言-
-	private Sprite[,] PuzzleBlockAry;
-	//Sprite格納用
-	private Sprite[] PuzzleSprite;
+	private PuzzleBlock[,] PuzzleBlockAry;
+
+	[SerializeField]
+	private PuzzleBlock _puzzleBlockPrefab;
+
+	[SerializeField]
+	private Transform _puzzleBlockParent;
 
 	//パズル列数は外部入力
 	public int PuzzleX = 1;
 	public int PuzzleY = 1;
 
-	//spriteのGameObjectを宣言
-	public GameObject gameObject;
-
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		//Inspectorで指定された縦＊横で二次元配列宣言
-		this.PuzzleBlockAry = new Sprite[this.PuzzleX,this.PuzzleY];
-		//[Piece]spriteをSliceした分格納
-		this.PuzzleSprite = Resources.LoadAll<Sprite> ("Piece");
+		this.PuzzleBlockAry = new PuzzleBlock[this.PuzzleX, this.PuzzleY];
+
+		//ブロックとブロックの隙間
+		float margin = 5f;
+
+		//ブロックの大きさ
+		float blockLength = 80f;
 
 		//宣言された二次元配列にランダムに[Piece]sprite格納
 		for (int i = 0; i < this.PuzzleY; i++) {
 			for (int j = 0; j < this.PuzzleX; j++) {
-				//PuzzleBlockAry配列に[Piece]spriteの中身をランダムに格納
-				this.PuzzleBlockAry [i, j] = this.PuzzleSprite[Random.Range(0,this.PuzzleSprite.Length-1)];
+				//PuzzleBlockプレハブを生成して親オブジェクトを指定する
+				PuzzleBlock pz = Instantiate (_puzzleBlockPrefab, _puzzleBlockParent);
+				//位置を決定
+				pz.transform.localPosition = new Vector2 (
+					i * (blockLength + margin),
+					j * (blockLength + margin)
+				);
+				//初期化する(色(0~4)情報を渡す、マス目位置を渡す)
+				pz.Init (Random.Range (0, 5), new Vector2 (i, j));
 			}
 		}
-
-		//Prefabを生成
-		this.gameObject = (GameObject)Resources.Load (this.PuzzleBlockAry [0, 0].name);
-
-		Debug.Log (this.PuzzleBlockAry[0,0].name);
-
-		GameObject go = Instantiate (gameObject) as GameObject;
-
-//		
-//		go.transform.position = new Vector2 (0, 0);
+		Debug.Log (this.PuzzleBlockAry [0, 0]);
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
 	}
 
-//	指定された要素数のパズルブロックを返す関数
-//	private void GetPuzzleBlock(int x,int y){
-//		this.PuzzleBlockAry = new Sprite [x, y];
-//	}
+	//	指定された要素数のパズルブロックを返す関数
+	//	private void GetPuzzleBlock(int x,int y){
+	//		this.PuzzleBlockAry = new Sprite [x, y];
+	//	}
 }
