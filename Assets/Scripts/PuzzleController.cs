66f30dbd42128c131f551117dc7947614011ab1b
hamada.kenji <hamada.kenji0925@gmail.com>
2017-06-26 22:38:02 +0900

    PuzzleSpinnerプロジェクト　PuzzleController.cs修正
    揃ったピースを削除し、上ブロックのピースを詰めて表示する処理を追加

--- Assets/Scripts/PuzzleController.cs
+++ Assets/Scripts/PuzzleController.cs
@@ -21,6 +21,12 @@ public class PuzzleController : MonoBehaviour
 	[SerializeField]
 	private int PuzzleY = 1;
 
+	//PuzzleのMaxピース数
+	private int maxPiece = 4;
+
+	//Puzzleの一致判定数
+	private int matchPuzzlePiece = 3;
+
 	//-SerchBlock関数-探索が終わった後のパズル座標を格納するListの宣言（後に得点計算などで使用するかも
 	private List<Vector2> searchAfterBlock = new List<Vector2>();
 
@@ -80,11 +86,13 @@ public class PuzzleController : MonoBehaviour
 				PuzzleBlock pz = Instantiate (_puzzleBlockPrefab, _puzzleBlockParent);
 				//位置を決定
 				pz.transform.localPosition = new Vector2 (
-					i * (blockLength + margin),
-					j * (blockLength + margin)
+					//i * (blockLength + margin),
+					//j * (blockLength + margin),
+					j * (blockLength + margin),
+					i * (blockLength + margin)
 				);
 				//初期化する(色(0~4)情報を渡す、マス目位置を渡す)
-				pz.Init (Random.Range (0, 5), new Vector2 (i, j));
+				pz.Init (Random.Range (0, maxPiece), new Vector2 (j, i));
 
 				//生成した情報を配列に格納
 				PuzzleBlockAry[i,j] = pz;
@@ -132,7 +140,8 @@ public class PuzzleController : MonoBehaviour
 		//引数で受け取ったGameObjectをVector2型、int型へ分解・代入
 		int selectX = (int)puzzleBlock.GetComponent<PuzzleBlock>().BlockPosition.x;
 		int selectY = (int)puzzleBlock.GetComponent<PuzzleBlock>().BlockPosition.y;
-		Vector2 judgePuzzleBlock = new Vector2 (selectY, selectX);
+		Vector2 judgePuzzleBlock = new Vector2 (selectX, selectY);
+		//Vector2 judgePuzzleBlock = new Vector2 (selectY, selectX);
 
 		int judgePuzzleColor = puzzleBlock.GetComponent<PuzzleBlock> ().ColorNum;
 
@@ -171,9 +180,9 @@ public class PuzzleController : MonoBehaviour
 
 					//判定したいパズル座標はパズル配列のMin・Max要素をオーバーしているか確認
 					if (targetIndex.x > PuzzleX - 1 ||
-					   targetIndex.x < 0 ||
-					   targetIndex.y > PuzzleY - 1 ||
-					   targetIndex.y < 0) {
+						targetIndex.x < 0 ||
+						targetIndex.y > PuzzleY - 1 ||
+						targetIndex.y < 0) {
 
 						//オーバーしていれば処理をスキップする
 						continue;
@@ -186,7 +195,7 @@ public class PuzzleController : MonoBehaviour
 
 					//軸ブロックの色と一致判定したいブロック色が同色か確認
 					if (judgePuzzleColor == targetColor) {
-						
+
 						//判定が終了したので一致座標を格納するListに追加
 						searchAfterBlock.Add(targetIndex);
 						//新たに軸として判定すべきパズル座標がある為、Listへ追加
@@ -204,8 +213,8 @@ public class PuzzleController : MonoBehaviour
 		//関数内で使用したListの初期化(searchAfterBlockはグローバル変数の為update内にてClear処理実施
 		searchPuzzleBlock.Clear ();
 
-		//一致したパズル数が３以上ならばtrueを返す
-		if (countPuzzle >= 3) {
+		//一致したパズル数がmatchPuzzlePiece以上ならばtrueを返す
+		if (countPuzzle >= matchPuzzlePiece) {
 			return true;
 		} else {
 			return false;
@@ -231,8 +240,11 @@ public class PuzzleController : MonoBehaviour
 			int blockAryY = (int)searchNormalBlock [i].y;
 
 			//該当するPuzzleBlockAry配列の画像に透明画像を設定する
-			PuzzleBlockAry [blockAryY, blockAryX].Init (5, new Vector2 (blockAryY, blockAryX));
+			//PuzzleBlockAry [blockAryY, blockAryX].Init (5, new Vector2 (blockAryY, blockAryX));
+			PuzzleBlockAry [blockAryY, blockAryX].Init (5, new Vector2 (blockAryX, blockAryY));
 		}
+
+		//for(int x = 0; x < )
 
 		//透明画像が存在している場合にloop
 		while (NotTransparent == false) {
@@ -250,13 +262,15 @@ public class PuzzleController : MonoBehaviour
 					//上ブロック色を取得
 					int upBlockColor = PuzzleBlockAry [blockY + 1, blockX].ColorNum;
 
+					//上のブロックが透明でなかった場合
 					if (upBlockColor != 5) {
 
 						//取得したブロック色を自ブロックに設定
-						block.Init (upBlockColor, new Vector2 (blockY, blockX));
-						//上ブロックの色を自ブロック色に設定(自ブロックは必ず透明(5)の為、直打ち設定
-						PuzzleBlockAry [blockY + 1, blockX].Init (5, new Vector2 (blockY, blockX));
+						block.Init (upBlockColor, new Vector2 (blockX, blockY));
+						//上ブロックの色を透明色に設定(自ブロックは必ず透明(5)の為、直打ち設定
+						PuzzleBlockAry [blockY + 1, blockX].Init (5, new Vector2 (blockX, blockY + 1));
 					} else {
+						//上のブロックが透明だった場合
 						for (int i = 0; i < (PuzzleY - 1); i++) {
 
 							//Y軸がオーバーフローして参照されないように条件指定する
@@ -265,11 +279,12 @@ public class PuzzleController : MonoBehaviour
 								//上ブロック色を取得
 								int BlockColor = PuzzleBlockAry [blockY + i, blockX].ColorNum;
 
+								//上のブロックが透明でなかった場合
 								if(BlockColor != 5){
 									//取得したブロック色を自ブロックに設定
-									block.Init (BlockColor, new Vector2 (blockY, blockX));
+									block.Init (BlockColor, new Vector2 (blockX, blockY));
 									//上ブロックの色を自ブロック色に設定(自ブロックは必ず透明(5)の為、直打ち設定
-									PuzzleBlockAry [blockY + i, blockX].Init (5, new Vector2 (blockY + i, blockX));
+									PuzzleBlockAry [blockY + i, blockX].Init (5, new Vector2 (blockX, blockY + i));
 								}
 							}
 
@@ -279,7 +294,7 @@ public class PuzzleController : MonoBehaviour
 
 					//確認するブロックのY軸が最大要素数か？ && ブロックカラーが透明か？　なら色をランダムに指定
 				} else if (blockY == (PuzzleY - 1) && block.ColorNum == 5) {
-					PuzzleBlockAry [blockY, blockX].Init (Random.Range (0, 5), new Vector2 (blockY, blockX));
+					PuzzleBlockAry [blockY, blockX].Init (Random.Range (0, maxPiece), new Vector2 (blockY, blockX));
 				}
 			}
 
@@ -291,139 +306,12 @@ public class PuzzleController : MonoBehaviour
 				if (pb.ColorNum != 5) {
 					NotTransparent = true;
 				} else {
-					Debug.Log ("一つでも透明ブロックが存在している");
 					//一つでも透明ブロックがあればフラグをfalse
 					NotTransparent = false;
-					continue;
+					break;
 				}
 			}
 
 		}
-
 	}
 }
-
-//			//PuzzleBlockAry要素数分loop
-//			foreach (PuzzleBlock block in PuzzleBlockAry) {
-//
-//				//比較の為、配列からX・Y軸、色番号を取得
-//				int blockX = (int)block.BlockPosition.x;
-//				int blockY = (int)block.BlockPosition.y;
-//				Debug.Log ("blockY;" + blockY);
-//				Debug.Log ("blockX;" + blockX);
-//
-//				//block自体のimgが透明か確認
-//				if (block.ColorNum == 5) {
-//
-//					//確認するブロックY軸が配列番号をオーバーしていないか確認
-//					if (blockY < PuzzleY - 1) {
-//
-//						//上座標ブロックの色を取得
-//						int upBlockColor = PuzzleBlockAry [blockY + 1, blockX].ColorNum;
-//
-//						//入れ替える上座標ブロックがある為、上ブロックの色を代入
-//						PuzzleBlockAry [blockY + 1, blockX].Init (block.ColorNum, new Vector2 (blockY + 1, blockX));
-//						block.Init (upBlockColor, new Vector2 (blockY, blockX)); 
-//					} else {
-//						//入れ替える上座標ブロックがない為、色をランダムに指定
-//						block.Init (Random.Range (0, 5), new Vector2 (blockY, blockX));
-//					}
-//				}
-//			}
-//
-//
-//			//配列をもう一度見直し、img透明が含まれていないことを確認する
-//			foreach (PuzzleBlock pb in PuzzleBlockAry) {
-//				//比較の為、配列からX・Y軸、色番号を取得
-//				int blockX = (int)pb.BlockPosition.x;
-//				int blockY = (int)pb.BlockPosition.y;
-//				int BlockColor = PuzzleBlockAry [blockY, blockX].ColorNum;
-//
-//				//img透明が存在するか確認
-//				if (BlockColor != 5) {
-//					NotTransparent = true;
-//				} else {
-//					//一つでも透明ブロックがあればフラグをfalse
-//					NotTransparent = false;
-//					continue;
-//				}
-//			}
-
-//		}
-
-//		//透明画像の存在フラグ
-//		bool NotTransparent = false;
-//		//loop回数カウント用
-//		int loopCount = 0;
-//
-//
-//		//透明画像が存在する限りloop
-//		while (NotTransparent == false) {
-//			
-//			//PuzzleBlockAryの要素数分をloop
-//			foreach (PuzzleBlock block in PuzzleBlockAry) {
-//
-//				//判定するブロック座標がY軸Maxではないか && 軸ブロックimgは透明か確認
-//				if (block.BlockPosition.y < PuzzleY - 1 && block.ColorNum == 5) {
-//						
-//					//上座標のブロックimgと透明ブロックimgの差し替え
-//					int blockX = (int)block.BlockPosition.x;
-//					int blockY = (int)block.BlockPosition.y;
-//					int upBlockColor = PuzzleBlockAry [blockY + 1, blockX].ColorNum;
-//					Debug.Log ("block座標：" + block.BlockPosition);
-//
-//					//上座標のブロックimgが透明ならloop
-////					while (upBlockColor == 5) {
-////						Debug.Log ("upBlockColor_loop");
-////						//透明ブロックimgと上ブロックimgの差し替えし
-////						PuzzleBlockAry [blockY, blockX].Init (upBlockColor, new Vector2 (blockY + loopCount, blockX));
-////						PuzzleBlockAry [blockY + 1, blockX].Init (5, new Vector2 (blockY, blockX));
-////
-////						//loopCountが指定したパズルY軸を越えれば
-////						if (loopCount > PuzzleY) {
-////
-////							//処理をスキップする
-////							continue;
-////						}
-////
-////						//loop回数カウント
-////						loopCount++;
-////				
-////					}
-//
-//					//上座標のブロックimgが透明か確認
-//					if (upBlockColor != 5) {
-//						//透明ブロックimgと上ブロックimgの差し替え
-//						PuzzleBlockAry [blockY, blockX].Init (upBlockColor, new Vector2 (blockY, blockX));
-//
-//						PuzzleBlockAry [blockY + 1, blockX].Init (5, new Vector2 (blockY + 1, blockX));
-//
-//					}
-//
-//				} else {
-//
-//					//差し替えるブロック座標が存在しない為、Randomでimg設定する
-//					Debug.Log("OK");
-//
-//				}
-//
-//			}
-//
-//			//img入れ替え後、透明imgは存在しないかを確認
-//			foreach (PuzzleBlock block in PuzzleBlockAry) {
-//
-//				//透明ブロックが存在しなければ
-//				if (block.ColorNum != 5) {
-//					NotTransparent = true;
-//				} else {
-//					NotTransparent = false;
-//				}
-//			}
-//
-//			//暫定処理
-//			NotTransparent = true;
-//		}
-//
-//	}
-//
-//}
